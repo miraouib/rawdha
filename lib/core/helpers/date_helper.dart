4097572a408b512558ab109f959d792d135f77a5
miraouib <miraoui.boubaker@gmail.com>
2026-01-27 15:37:14 +0100

    date name arab

--- lib/core/helpers/date_helper.dart
+++ lib/core/helpers/date_helper.dart
@@ -2,25 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 class DateHelper {
+  static final Map<int, String> _tunisianMonths = {
+    1: 'جانفي',
+    2: 'فيفري',
+    3: 'مارس',
+    4: 'أفريل',
+    5: 'ماي',
+    6: 'جوان',
+    7: 'جويلية',
+    8: 'أوت',
+    9: 'سبتمبر',
+    10: 'أكتوبر',
+    11: 'نوفمبر',
+    12: 'ديسمبر',
+  };
+
+  static final Map<int, String> _arabicWeekdays = {
+    1: 'الإثنين',
+    2: 'الثلاثاء',
+    3: 'الأربعاء',
+    4: 'الخميس',
+    5: 'الجمعة',
+    6: 'السبت',
+    7: 'الأحد',
+  };
+
   static String formatMonthYear(BuildContext context, DateTime date) {
+    if (context.locale.languageCode == 'ar') {
+      return convertNumbers(context, '${_tunisianMonths[date.month]} ${date.year}');
+    }
     String formatted = DateFormat('MMMM yyyy', 'fr').format(date);
     return convertNumbers(context, formatted);
   }
 
   static String formatDateShort(BuildContext context, DateTime date) {
+    if (context.locale.languageCode == 'ar') {
+      return convertNumbers(context, '${date.day} ${_tunisianMonths[date.month]}');
+    }
     String formatted = DateFormat('dd MMM', 'fr').format(date);
     return convertNumbers(context, formatted);
   }
 
   static String formatDateLong(BuildContext context, DateTime date) {
+    if (context.locale.languageCode == 'ar') {
+      return convertNumbers(context, '${date.day} ${_tunisianMonths[date.month]} ${date.year}');
+    }
     String formatted = DateFormat('dd MMMM yyyy', 'fr').format(date);
     return convertNumbers(context, formatted);
   }
 
   static String formatDateFull(BuildContext context, DateTime date) {
+    if (context.locale.languageCode == 'ar') {
+      return convertNumbers(context, '${_arabicWeekdays[date.weekday]} ${date.day} ${_tunisianMonths[date.month]} ${date.year}');
+    }
     String formatted = DateFormat('EEEE d MMMM yyyy', 'fr').format(date);
     // Capitalize first letter
-    formatted = formatted[0].toUpperCase() + formatted.substring(1);
+    if (formatted.isNotEmpty) {
+      formatted = formatted[0].toUpperCase() + formatted.substring(1);
+    }
     return convertNumbers(context, formatted);
   }
 
@@ -31,7 +70,7 @@ class DateHelper {
 
     String result = text;
     for (int i = 0; i < arabicDigits.length; i++) {
-        result = result.replaceAll(arabicDigits[i], westernDigits[i]);
+      result = result.replaceAll(arabicDigits[i], westernDigits[i]);
     }
     return result;
   }
